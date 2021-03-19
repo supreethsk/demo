d734ed9d3806446973a3cf8838fe3783cb8906dc
supreethsk <supreeth1997@gmail.com>
2021-03-19 12:50:03 +0530

    pavi

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-
 import './App.css';
 
 function App() {
   return (
     <div className="App">
      <center>
-       <h2>Welcome</h2>
+       <h2>pavitra Welcome</h2>
      </center>
     </div>
   );
